88513b12d0948e58f6b8580974d326ce05b87efe
Martin Kopecký <kopecky@thekrew.app>
2024-12-04 16:18:45 +0100

    day 5 prep

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"aoc2024/day02"
 	"aoc2024/day03"
 	"aoc2024/day04"
+	"aoc2024/day05"
 )
 
 func main() {
@@ -13,4 +14,5 @@ func main() {
 	aoc.Run(2, day02.Solution{})
 	aoc.Run(3, day03.Solution{})
 	aoc.Run(4, day04.Solution{})
+	aoc.Run(5, day05.Solution{})
 }
