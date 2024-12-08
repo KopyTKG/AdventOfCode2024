0e3ab031d1dea36ce5bda800949d36e8ccd223e2
Martin Kopecký <kopecky@thekrew.app>
2024-12-08 13:26:54 +0100

    welp

---
+++ day08/day08.go
@@ -0,0 +1,13 @@
+package day08
+
+import "aoc2024/aoc"
+
+type Solution struct{}
+
+func (s Solution) Star1(input string) (string, error) {
+	return "", aoc.NIP()
+}
+
+func (s Solution) Star2(input string) (string, error) {
+	return "", aoc.NIP()
+}
--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"aoc2024/aoc"
-	"aoc2024/day07"
+	"aoc2024/day08"
 )
 
 func main() {
@@ -12,5 +12,6 @@ func main() {
 	// aoc.Run(4, day04.Solution{})
 	// aoc.Run(5, day05.Solution{})
 	// aoc.Run(6, day06.Solution{})
-	aoc.Run(7, day07.Solution{})
+	// aoc.Run(7, day07.Solution{})
+	aoc.Run(8, day08.Solution{})
 }
